b1535af75e18feac38af577cb754ae19723b0625
labtel <a20221862@pucp.edu.pe>
2026-04-09 20:30:26 -0500

    push

---
+++ src/main/java/com/example/lab120221862/controller/Clientes.java
@@ -0,0 +1,19 @@
+package com.example.lab120221862.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/clientela", method = RequestMethod.GET)
+public class Clientes {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String formularioCliente(){
+        return "Cliente / crear";
+    }
+    @RequestMapping(value = "/guardar", method = RequestMethod.GET)
+    public String guardarCliente(){
+        return "redirect: /clientela";
+}
--- src/main/java/com/example/lab120221862/controller/HomeController.java
+++ src/main/java/com/example/lab120221862/controller/HomeController.java
@@ -6,10 +6,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(value = "/carta", method = RequestMethod.GET)
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String paginaPrincipal(){
-        return "Hola munda";
+        return "Pedido";
     }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String listarPlatos(){
+        return "Platos";
+    }
+
+    @RequestMapping(value = "crearFrm", method = RequestMethod.GET)
+    public String formularioPlatos(){
+        return "Platos / crear";
+    }
+
+    @RequestMapping(value = "/guardar", method = RequestMethod.GET)
+    public String guardarPlatos(){
+        return "redirect: /carta";
+    }
+
 }
