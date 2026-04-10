a8e4c60089cca34f8d2678bc74ce9596515202ad
labtel <a20221862@pucp.edu.pe>
2026-04-09 19:47:35 -0500

    first commit

---
+++ src/main/java/com/example/lab120221862/Lab120221862Application.java
@@ -0,0 +1,13 @@
+package com.example.lab120221862;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab120221862Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab120221862Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab120221862/Lab120221862ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab120221862;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab120221862ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
